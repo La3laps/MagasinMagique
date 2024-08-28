21f093dd2945685662307c9ec63d69555ce19020
remi.legrand <remi.legrand@le-campus-numerique.fr>
2024-08-28 16:02:44 +0200

    Tests pour l'objet comté

--- src/main/java/com/magasin/Item.java
+++ src/main/java/com/magasin/Item.java
@@ -4,9 +4,9 @@ public class Item {
 
     public String name;
 
-    public int sellIn;
+    public int sellIn; // nombre de jours qui reste
 
-    public int quality;
+    public int quality; // quality change tous les jours
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -1,5 +1,6 @@
 package com.magasin;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +8,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MagasinTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    @DisplayName("Tests de Rémi")
+    void comteCheese() {
+        Item[] items = new Item[] { new Item("Comté", 4, 10) };
         Magasin app = new Magasin(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(3, app.items[0].sellIn);
+        assertEquals(11, app.items[0].quality);
     }
 
+    @Test
+    @DisplayName("n°1")
+    void comteCheese1() {
+        Item[] items = new Item[] { new Item("Comté", 0, 0) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(2, app.items[0].quality);
+    }
+
+    @Test
+    @DisplayName("n°2")
+    void comteCheese2() {
+        Item[] items = new Item[] { new Item("Comté", 7, 50) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(6, app.items[0].sellIn);
+        assertEquals(50, app.items[0].quality);
+    }
 }
